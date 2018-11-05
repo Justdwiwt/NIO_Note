f6eff91af05f286aa34cce547c6ad624c04a39b8
Justdwiwt <len_master@hotmail.com>
2018-11-05 08:36:17 +0800

    update NIO note

--- src/main/TestDemo.java
+++ src/main/TestDemo.java
@@ -10,9 +10,8 @@ public class TestDemo {
     @Test
     public void testArrayQueue() throws InterruptedException {
         BlockingQueue<Object> queue = new ArrayBlockingQueue<>(10);
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++)
             queue.add(i);
-        }
 //        queue.addAll(11);
         queue.put(11);
         System.out.println(queue.offer(11));
